697067bdefa995a67ba6c6ecdb6ce102ae5df2ef
Dinara Safina <dinara.safina.gdc@ts.fujitsu.com>
2021-03-15 22:23:54 +0300

    Лекция 3 часть 1

--- addressbook-web-tests/addressbook-web-tests/AppManager/ContactHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/ContactHelper.cs
@@ -62,14 +62,10 @@ namespace WebAddressbookTests
 
         public ContactHelper FillContactForm(ContactData contact)
         {
-            driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
-            driver.FindElement(By.Name("middlename")).Clear();
-            driver.FindElement(By.Name("middlename")).SendKeys(contact.Middlename);
-            driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
-            driver.FindElement(By.Name("address")).Clear();
-            driver.FindElement(By.Name("address")).SendKeys(contact.Address);
+            Type(By.Name("firstname"), contact.Firstname);
+            Type(By.Name("middlename"), contact.Middlename);
+            Type(By.Name("lastname"), contact.Lastname);
+            Type(By.Name("address"), contact.Address);
             //new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText("15");
             //driver.FindElement(By.XPath("//option[@value='15']")).Click();
             //new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText("November");
--- addressbook-web-tests/addressbook-web-tests/Tests/TestBase.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/TestBase.cs
@@ -17,15 +17,7 @@ namespace WebAddressbookTests
         [SetUp]
         public void SetupTest()
         {
-            app = new ApplicationManager();
-            app.Navigator.OpenHomePage();
-            app.Auth.Login(new AccountData("admin", "secret"));
+            app = ApplicationManager.GetInstance();
         }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            app.Stop();
-        }        
     }
 }
